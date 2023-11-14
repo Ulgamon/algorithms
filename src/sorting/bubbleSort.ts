61522318989870ee5631000784e002f72540f319
Ulgamon <justinilic1@gmail.com>
2023-11-14 10:27:51 +0100

    bubbleSort

---
+++ src/sorting/bubbleSort.ts
@@ -0,0 +1,13 @@
+export const bubbleSort = (array: Array<number>) => {
+  let temp = 0;
+  for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        temp = array[j + 1];
+        array[j + 1] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  return array;
+};
---
+++ test/sorting/bubbleSort.test.ts
@@ -0,0 +1,9 @@
+import { bubbleSort } from "../../src/sorting/bubbleSort";
+
+describe("Bubble Sort Algorithm", () => {
+  it("array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0] needs to be sorted ascending.", () => {
+    expect(bubbleSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])).toEqual([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    ]);
+  });
+});
