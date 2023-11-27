f420364338b7f8feb298741acf251cf13a7b70b8
Ulgamon <justinilic1@gmail.com>
2023-11-27 10:06:19 +0100

    areThereDuplicates frequency counter

---
+++ src/freq-counter/are-there-duplicates.ts
@@ -0,0 +1,21 @@
+// Frequency Counter - areThereDuplicates
+// Implement a function called areThereDuplicates which accepts a variable number of arguments,
+// and checks whether there are any duplicates among the arguments passed in.
+
+// Restrictions: Time Complexity - O(n), Space Complexity - O(n)
+
+function areThereDuplicates(...args: any) {
+  if (args.length === 0) return;
+
+  let dict: any = {};
+
+  for (let i = 0; i < args.length; i++) {
+    if (dict[args[i]]) {
+      return true;
+    } else {
+      dict[args[i]] = 1;
+    }
+  }
+
+  return false;
+}
